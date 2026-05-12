3e0c901afd76053a6c49b52ca71ebff89f81e833
siddharthbhadouriya <bhadouriya.siddharth2003@gmail.com>
2026-05-13 01:28:29 +0530

    added API connection in main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from pipeline.intent import extract_intent
+from pipeline.design import design_system
 
-app = FastAPI()
+class CompileRequest(BaseModel):
+    prompt: str
+
+
+
+app = FastAPI()  #app works as controller of server
+
+@app.post("/compile")
+def compile_app(request : CompileRequest):
+    intent = extract_intent(request.prompt)
+    design = design_system(intent)
+
+    return {
+        "intent": intent,
+        "design": design,
+    }
+
+
 
 @app.get("/")
 def home():
